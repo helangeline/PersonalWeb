5cc07fd9dbf69e6d1b48a6cd3afcc6dcf12a32ed
helangeline <40053340+helangeline@users.noreply.github.com>
2022-03-12 16:46:56 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Angeline Budinata</h1>
+      <div>I like to code (lol.)</div>
+      <div></div>
+    </div>
+  );
+}
